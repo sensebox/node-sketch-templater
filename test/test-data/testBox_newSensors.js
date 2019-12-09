a5f4738414b942b9ec700ad345f4f756e7a08a63
felixerdy <felix.erdmann@uni-muenster.de>
2019-12-09 13:51:39 +0100

    add test for new transformer

---
+++ test/test-data/testBox_newSensors.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const randomHex = function randomHex() {
+  return Buffer.from(Math.random().toString(36)).toString('hex');
+};
+
+module.exports = function testBox() {
+  return {
+    _id: randomHex(),
+    sensors: [
+      {
+        title: 'Temperatur',
+        _id: randomHex(),
+        sensorType: 'HDC1080'
+      },
+      {
+        title: 'Luftdruck',
+        _id: randomHex(),
+        sensorType: 'BMP280'
+      },
+      {
+        title: 'Kellertemperatur',
+        _id: randomHex(),
+        sensorType: 'HDC1080'
+      }
+    ],
+    serialPort: 'Serial1',
+    soilDigitalPort: 'B',
+    soundMeterPort: 'C',
+    bme680Phenomenon: 'VOC',
+    ssid: 'MY-HOME-NETWORK',
+    password: 'MY-SUPER-PASSWORD'
+  };
+};
